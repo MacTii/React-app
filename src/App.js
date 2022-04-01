b4f5fb08e325f78f6c4d41082e548528dcedf120
MacTii <mactii41@gmail.com>
2022-04-01 15:36:22 +0200

    Add routes to pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from 'react'
 import Navbar from "./components/Navbar/Navbar"
-import './App.css';
+import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import SignUp from './components/pages/SignUp'
+import Products from './components/pages/Products'
+import ContactUs from './components/pages/ContactUs'
+import Services from './components/pages/Services'
+import Home from './components/pages/Home'
 
 function App() {
   return (
     <div className='App'>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='signup' element={<SignUp />} />
+          <Route path='products' element={<Products />} />
+          <Route path='contactus' element={<ContactUs />} />
+          <Route path='services' element={<Services />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
